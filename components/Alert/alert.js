f008a471ac98d2d42864e36f5fb99e19740fe8f3
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2022-01-29 22:21:00 +0530

    feat: added functionality to close icon

---
+++ components/Alert/alert.js
@@ -0,0 +1,6 @@
+const alertClose = document.querySelector(".alert-close");
+const close = document.querySelector(".close");
+
+close.addEventListener("click", ()=> {
+    alertClose.style.display = "none";
+})
