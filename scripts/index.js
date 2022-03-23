7fa9ea04495442dbf2977d663312b102ebb6e485
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2022-03-23 23:22:56 +0530

    feat:added links for search

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
 let searchLinks = document.querySelector(".search-links");
+let backgroundTransparent = document.querySelector(".background-transparent");
 let search = document.querySelector(".search");
 
 const showSearchLinks = () => {
     searchLinks.classList.remove("hide");
+    backgroundTransparent.classList.remove("hide");
 }
 
 
 search.addEventListener("click",showSearchLinks);
+
+
+window.onclick = function(e) {
+    if (e.target == backgroundTransparent) {
+        backgroundTransparent.classList.add("hide");
+        searchLinks.classList.add("hide");
+    }
+  }
